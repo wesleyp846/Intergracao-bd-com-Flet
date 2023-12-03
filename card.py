3918ed947cdc54219235283449b5eed6f7369e84
Wesley Pereira <101286798+wesleyp846@users.noreply.github.com>
2023-12-03 22:12:50 +0000

    card constrution

--- card.py
+++ card.py
@@ -1,30 +1,32 @@
 import flet as ft
 
-def card
+def card(contato, tel):
+    return ft.Container(
+        content=ft.Column(
+            [
+                ft.ListTile(
+                    leading=ft.Icon(ft.icons.CONTACT_PHONE),
+                    title=ft.Text(contato),
+                    subtitle=ft.Text(
+                        tel
+                    ),
+                ),
+                ft.Row(
+                    [ft.TextButton('Ver contato'), ft.TextButton('Voltar')],
+                    alignment=ft.MainAxisAlignment.END,
+                ),
+            ]
+        ),
+        width=400,
+        padding=10,
+    )
+
 
 def main(page):
     page.title = 'Exemplo de card'
     page.add(
         ft.Card(
-            content=ft.Container(
-                content=ft.Column(
-                    [
-                        ft.ListTile(
-                            leading=ft.Icon(ft.icons.CONTACT_PHONE),
-                            title=ft.Text('Nome do Contato'),
-                            subtitle=ft.Text(
-                                'Número do contato.'
-                            ),
-                        ),
-                        ft.Row(
-                            [ft.TextButton('Ver contato'), ft.TextButton('Voltar')],
-                            alignment=ft.MainAxisAlignment.END,
-                        ),
-                    ]
-                ),
-                width=400,
-                padding=10,
-            )
+            card('Nome do contato', 'Telefone da pessoa')
         )
     )
 
@@ -54,6 +56,4 @@ ft.app(target=main, view=ft.AppView.WEB_BROWSER)
 #     card.update()
 
 # def main(page):  
-#     page.add(Card())   
-
-#ft.app(target=main, view=ft.AppView.WEB_BROWSER)
+#     page.add(Card())
