dd6cbbf332b9fc38d0c45a19d5ed76a45143f207
Wesley Pereira <aventureiro-msn@hotmail.com>
2023-12-08 16:30:11 -0300

    helping

--- ajuda.py
+++ ajuda.py
@@ -20,12 +20,86 @@ class App(ft.UserControl):
         self.adicionar_dados=ft.TextField(label='Nome do dado')
         self.editar_dados=ft.TextField(label='Editar')
     
+    def deletar(self, x, y):
+        cursor.execute('DELETE FROM clientes WHERE id = ?', [x])
+        y.open=False
+
+        #chamando a funçao render de renderizar dados
+        self.todos_dados.controls.clear()
+        self.renderizar_todos()
+        self.page.update()
+    
+    def atualizar(self, x , y, z):
+        cursor.execute('UPDATE clientes SET nome = ? WHERE id = ?', (y, x))
+        conexao.commit()
+
+        z.open=False
+
+        #chamando a funçao render de renderizar dados
+        self.todos_dados.controls.clear()
+        self.renderizar_todos()
+        self.page.update()
+
+    def abrir_acoes(self, e):
+    #criando a função para abrir as acoes
+        id_user=e.controls.subtitle.value
+        self.editar_dados.value = e.controls.title.value
+        self.update()
+
+        alerta_dialogo = ft.AlertDialog(
+            title=ft.Text(f'Editar ID {id_user}'),
+            content=self.editar_dados,
+
+            #botoes de acao
+            actions=[
+                ft.ElevatedButton(
+                    "Deletar",
+                    color=ft.colors.WHITE,
+                    bgcolor=ft.colors.RED,
+                    on_click= lambda e: self.deletar(id_user, alerta_dialogo)
+                ),
+                ft.ElevatedButton(
+                    'Atualizar',
+                    on_click=lambda e: self.atualizar(id_user, self.editar_dado.value, alerta_dialogo)
+                )
+            ],
+            actions_alignment=ft.MainAxisAlignment.SPACE_BETWEEN
+        )
+
+        self.page.dialog = alerta_dialogo
+        alerta_dialogo.open=True
+
+        #para atualizar a pagina
+        self.page.update()
+    
     def renderizar_todos(self):
-        pass
+    #Mostrar todos os dados do banco de dados
+        cursor.execute('SELECT * FROM clientes')
+        conexao.commit()
+        print('foi chamada renderizar_todos')
+
+        meus_dados = cursor.fetchall()
+
+        for dado in meus_dados:
+            #utilizo sempre o controls quando utilizo a super class Usercontrols
+            
+            self.todos_dados.controls.append(
+                ft.ListTile(
+                    title=ft.Text(dado[1]),
+                    subtitle=ft.Text(dado[0]),
+                    on_click=self.abrir_acoes
+                )
+            )
+        self.update()
+
+    def ciclo(self):
+        #nao deixa o banco feochar a conecxao
+        self.renderizar_todos()
 
     def adicionar_novo_dado(self, e):
         cursor.execute('INSERT INTO clientes (nome) VALUES (?)',
             [self.adicionar_dados.value])
+        conexao.commit()
         
         self.todos_dados.controls.clear()
         self.renderizar_todos()
@@ -37,19 +111,20 @@ class App(ft.UserControl):
             self.adicionar_dados,
             ft.ElevatedButton('Adicionar dado',
                 on_click=self.adicionar_novo_dado),
-            self.todos_dados        
+            self.todos_dados,     
         ])
-    
+
 def main(page: ft.Page):
+    page.update()
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.title = "Cadastro"
     page.padding = 10
     page.window_width = 400
     page.window_height = 650
-    page.theme_mode = ft.ThemeMode.DARK  
+    page.theme_mode = ft.ThemeMode.DARK
 
+    tabela_base()
     minha_aplicacao=App()
-    
 
     page.add(
         minha_aplicacao,     
