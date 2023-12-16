e6dbcf01f2171e7b2ea50ae70fd7f1f19f7e5cc8
Wesley Pereira <aventureiro-msn@hotmail.com>
2023-12-16 20:40:51 -0300

    nova versao

--- refatorando.py
+++ refatorando.py
@@ -3,37 +3,23 @@ from flet import *
 from classes import Dados
 
 casa = True
+
 def main(page: ft.Page):
     page.title = "Agenda de Contatos"
     page.padding = 10
     page.window_width = 400
     page.window_height = 650
     page.theme_mode = ft.ThemeMode.DARK
+    page.scroll = ft.ScrollMode.AUTO
 
-    db=Dados('banco.db')
+    db = Dados('banco.db')
 
-    def deleta_contato(e):
-        #print(f"Apagar contato: {e.control.content.value}")
-        #print(f"Apagar contato: {contato}")
-        #contato = e.control.parent.title.value 
-        #contato = listagem_contatos.controls.Card.title.value
-        #contato = listagem_contatos.controls[0].content.content.controls[0]
-        contato = listagem_contatos.controls[0].content.content.controls[0].controls[0].value
-        print(f"Apagar contato: {contato}")
-   
-    barra_menu = Text(
-        'Todos os elatede botes aqui', 
-        size=20, 
-        weight='bold',
-        bgcolor='red'
-    )
+    def renderizar_contatos():
+        listagem_contatos.controls.clear()
+        dados_lidos = db.ler_dados()
 
-    listagem_contatos = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
-    dados_lidos = db.ler_dados()
-    
-    for lido in dados_lidos:
-        listagem_contatos.controls.append(
-            Card(
+        for lido in dados_lidos:
+            card_contato = Card(
                 Container(
                     content=Column([
                         ft.ExpansionTile(
@@ -50,25 +36,72 @@ def main(page: ft.Page):
                             ],
                         ),
                         ft.Row([
-                            #para apagar o dado é o nome
-                            ft.TextButton("Editar"), 
+                            ft.TextButton(
+                                "Editar",
+                                on_click=lambda _, nome=lido[1]: edita_contato(nome),
+                            ), 
                             ft.TextButton(
                                 "Apagar",
-                                on_click = deleta_contato, #lambda _: db.deletar_dados(lido[1]),
+                                on_click=lambda _, nome=lido[1]: deleta_contato(nome),
                             ),
-                        ],alignment=ft.MainAxisAlignment.END,
-                        ),
-                    ]
-                ),
-                width=400,
-                padding=10,
+                        ],
+                        alignment=ft.MainAxisAlignment.END),
+                    ]),
+                    width=400,
+                    padding=10,
+                )
             )
+            listagem_contatos.controls.append(card_contato)
+        page.update()
+
+    def deleta_contato(nome):
+        db = Dados('banco.db')
+        db.deletar_dados(nome)
+        print("fazer uma snakBar avisando que o contato: {nome} foi excluido")
+        renderizar_contatos()
+
+    def edita_contato(nome):
+        print(f'fazer a edição de {nome}')
+
+    barra_inicial = ft.Row([
+        Icon(
+            icons.MENU_BOOK, 
+            color='blue600',
+            size=60,
+        ),
+        Text(
+            'AGENDA', 
+            size=30, 
+            weight='bold',
         )
+    ],
+    alignment=ft.MainAxisAlignment.CENTER
+    )
+
+    listagem_contatos = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
+
+    renderizar_contatos()
+
+    page.navigation_bar = NavigationBar(
+        destinations=[
+            NavigationDestination(
+                icon=icons.CONTACT_PHONE_SHARP, 
+                label="Add Contato"
+            ),
+            NavigationDestination(
+                icon=icons.KEYBOARD_DOUBLE_ARROW_UP_SHARP, 
+                label="Importar"
+            ),
+            NavigationDestination(
+                icon=icons.KEYBOARD_DOUBLE_ARROW_DOWN_SHARP, 
+                label="Exportar"
+            ),
+        ]
     )
 
     page.add(
         Column([
-            barra_menu,
+            barra_inicial,
             listagem_contatos,
         ])
     )
